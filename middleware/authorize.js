3ae50d3c24a5c24873f48468e5db173869d8f426
shehab <shehab.mohey54@gmail.com>
2023-04-09 15:17:50 +0200

    modified

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,7 +1,7 @@
 const conn=require('../db/connection');
 const util = require ("util");
 
-const authorized =async (req,res)=>{
+const authorized =async (req,res,next)=>{
     const query =util.promisify(conn.query).bind(conn);
     const {token} = req.headers;
     const user = await query("select * from user_model where token = ?");
