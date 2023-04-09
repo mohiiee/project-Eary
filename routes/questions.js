bed93dc32d8166df5478697ac2cbbf630615ceef
haidy <haidygad1234@gmail.com>
2023-04-09 20:54:14 +0200

    modified

--- routes/questions.js
+++ routes/questions.js
@@ -1,23 +1,37 @@
 const router = require("express").Router();
 const con=require('../db/connection');
 const authorized=require("../middleware/authorize");
+const admin=require("../middleware/admin");
+const{ body, validationResult}=require("express-validator");
+const upload=require("../middleware/uploadaudio");
+
 //admin [create ,update,delet list(get all) ] 
-router.post("/create",authorized,(req,res)=>{
+//Create The API
+router.post("/create",admin,
+upload.single("Audio"),
+body("Name").isString().withMessage("Please Enter your Question Name").isLength({min:8,max:20}).withMessage("Question Name should be at least 8 character"),
+body("Questions").isString().withMessage("Please Enter your Questions").isLength({min:8,max:200}).withMessage("Questions should be at least 8 character"),
+authorized,(req,res)=>{
+     //validdation request // law fe error hyrg3aly 
+     const errors=validationResult(req);
+     if (!errors.isEmpty()){
+         return res.status(400).json({error:errors.array()});
+     }
     res.status(200).json({
-        msg:"question created",
+        msg:req.body,
     });
 });
-router.put("/update",authorized,(req,res)=>{
+router.put("/update",admin,authorized,(req,res)=>{
     res.status(200).json({
         msg:"question updated",
     });
 });
-router.delete("/delete",authorized,(req,res)=>{
+router.delete("/delete",admin,authorized,(req,res)=>{
     res.status(200).json({
         msg:"question deleted",
     });
 });
-router.get("/list",(req,res)=>{
+router.get("/list",authorized,(req,res)=>{
     res.status(200).json({
         questions:[],
     });
@@ -25,7 +39,7 @@ router.get("/list",(req,res)=>{
 
 //user[list , answer]
 
-router.post("/answer",(req,res)=>{
+router.post("/answer",authorized,(req,res)=>{
     res.status(200).json({
         msg:"answers added",
     });
