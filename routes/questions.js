ab92bf713a127bf5f099430dcf07cbd44a67d584
haidy <haidygad1234@gmail.com>
2023-04-10 06:11:07 +0200

    modified

--- routes/questions.js
+++ routes/questions.js
@@ -11,7 +11,7 @@ router.post("/create",admin,
 upload.single("Audio"),
 body("Name").isString().withMessage("Please Enter your Question Name").isLength({min:8,max:20}).withMessage("Question Name should be at least 8 character"),
 body("Questions").isString().withMessage("Please Enter your Questions").isLength({min:8,max:200}).withMessage("Questions should be at least 8 character"),
-authorized,(req,res)=>{
+(req,res)=>{
      //validdation request // law fe error hyrg3aly 
      const errors=validationResult(req);
      if (!errors.isEmpty()){
@@ -21,12 +21,12 @@ authorized,(req,res)=>{
         msg:req.body,
     });
 });
-router.put("/update",admin,authorized,(req,res)=>{
+router.put("/update",admin,(req,res)=>{
     res.status(200).json({
         msg:"question updated",
     });
 });
-router.delete("/delete",admin,authorized,(req,res)=>{
+router.delete("/delete",admin,(req,res)=>{
     res.status(200).json({
         msg:"question deleted",
     });
